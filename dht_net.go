0a07fc9150680e8a140d65564f68ba7edc7c39a2
vyzo <vyzo@hackzen.org>
2017-09-17 13:00:57 +0300

    dht_net: more consistent handling of retry resets; check for streamReuseTries  after increment

--- dht_net.go
+++ dht_net.go
@@ -286,10 +286,11 @@ func (ms *messageSender) SendMessage(ctx context.Context, pmes *pb.Message) erro
 
 		log.Event(ctx, "dhtSentMessage", ms.dht.self, ms.p, pmes)
 
-		if ms.singleMes > streamReuseTries {
-			ms.resetHard()
-		} else if retry {
+		if retry {
 			ms.singleMes++
+			if ms.singleMes > streamReuseTries {
+				ms.resetHard()
+			}
 		}
 
 		return nil
@@ -386,10 +387,9 @@ func (ms *messageSender) SendRequest(ctx context.Context, pmes *pb.Message) (*pb
 
 		if retry {
 			ms.lk.Lock()
+			ms.singleMes++
 			if ms.singleMes > streamReuseTries {
 				ms.resetSoft(rcount)
-			} else {
-				ms.singleMes++
 			}
 			ms.lk.Unlock()
 		}

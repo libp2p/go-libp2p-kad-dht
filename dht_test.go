ed147fe8858f68020b5ca680a6002a8ea9db5a93
Dirk McCormick <dirkmdev@gmail.com>
2018-01-11 04:08:02 +0800

    Use record ValidationRecord in tests

--- dht_test.go
+++ dht_test.go
@@ -20,7 +20,6 @@ import (
 	peer "github.com/libp2p/go-libp2p-peer"
 	pstore "github.com/libp2p/go-libp2p-peerstore"
 	record "github.com/libp2p/go-libp2p-record"
-	recpb "github.com/libp2p/go-libp2p-record/pb"
 	bhost "github.com/libp2p/go-libp2p/p2p/host/basic"
 	ci "github.com/libp2p/go-testutil/ci"
 	travisci "github.com/libp2p/go-testutil/ci/travis"
@@ -51,7 +50,7 @@ func setupDHT(ctx context.Context, t *testing.T, client bool) *IpfsDHT {
 	}
 
 	d.Validator["v"] = &record.ValidChecker{
-		Func: func(*recpb.Record) error {
+		Func: func(*record.ValidationRecord) error {
 			return nil
 		},
 		Sign: false,
@@ -151,7 +150,7 @@ func TestValueGetSet(t *testing.T) {
 	defer dhtB.host.Close()
 
 	vf := &record.ValidChecker{
-		Func: func(*recpb.Record) error { return nil },
+		Func: func(*record.ValidationRecord) error { return nil },
 		Sign: false,
 	}
 	nulsel := func(_ string, bs [][]byte) (int, error) { return 0, nil }

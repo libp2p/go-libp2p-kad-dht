9ff43d6ead80cd1dcead6bec24de49c3baca0d5f
Steven Allen <steven@stebalien.com>
2018-04-16 14:25:00 +0900

    note that the peerstore attempts to extract the key from the peer id

--- records.go
+++ records.go
@@ -26,7 +26,8 @@ type pubkrs struct {
 func (dht *IpfsDHT) GetPublicKey(ctx context.Context, p peer.ID) (ci.PubKey, error) {
 	log.Debugf("getPublicKey for: %s", p)
 
-	// check locally.
+	// Check locally. Will also try to extract the public key from the peer
+	// ID itself if possible (if inlined).
 	pk := dht.peerstore.PubKey(p)
 	if pk != nil {
 		return pk, nil

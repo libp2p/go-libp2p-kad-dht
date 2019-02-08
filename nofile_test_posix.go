da7209a6532a328e9f725142d30668ce040ca867
Steven Allen <steven@stebalien.com>
2019-02-07 17:33:57 -0800

    test: fix tests on freebsd

--- nofile_test_posix.go
+++ nofile_test_posix.go
@@ -7,5 +7,6 @@ import "syscall"
 func curFileLimit() uint64 {
 	var n syscall.Rlimit
 	syscall.Getrlimit(syscall.RLIMIT_NOFILE, &n)
-	return n.Cur
+	// cast because some platforms use int64 (e.g., freebsd)
+	return uint64(n.Cur)
 }

95256379dfdd253f694d26f715f0168ac55ba267
Matt Joiner <anacrolix@gmail.com>
2019-02-13 11:53:42 +1100

    Revert "Update for dht v5"
    This reverts commit fb417eb418d2a59ecca3f14e29d3d74aca41a43e.

--- cmd/dht-tool/main.go
+++ cmd/dht-tool/main.go
@@ -21,7 +21,7 @@ import (
 	"github.com/anacrolix/ipfslog"
 
 	libp2p "github.com/libp2p/go-libp2p"
-	dht "github.com/libp2p/go-libp2p-kad-dht/v5"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
 )
 
 func main() {

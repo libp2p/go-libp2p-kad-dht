0f5682aae31bf84e9d748611e5cd90ea085a52e2
Matt Joiner <anacrolix@gmail.com>
2019-03-28 17:20:56 +1100

    Include branch label on histograms

--- prometheus.go
+++ prometheus.go
@@ -22,6 +22,8 @@ const (
 	localPeerId = "local_peer_id"
 )
 
+var constLabels = prometheus.Labels{"branch": "stream-pooling-with-metrics"}
+
 // See https://groups.google.com/d/msg/prometheus-developers/ntZHQz216c0/DSbqaA-4EwAJ
 func newGaugeFunc(name string, f func() float64, labels prometheus.Labels) prometheus.GaugeFunc {
 	opts := prometheus.GaugeOpts(newOpts(name))
@@ -29,21 +31,23 @@ func newGaugeFunc(name string, f func() float64, labels prometheus.Labels) prome
 	return promauto.NewGaugeFunc(opts, f)
 }
 
+// Don't forget to also change newHistogramOpts.
 func newOpts(name string) prometheus.Opts {
 	return prometheus.Opts{
 		Namespace:   namespace,
 		Subsystem:   subsystem,
 		Name:        name,
-		ConstLabels: prometheus.Labels{"branch": "stream-pooling-with-metrics"},
+		ConstLabels: constLabels,
 	}
 }
 
 func newHistogramOpts(name string, buckets []float64) prometheus.HistogramOpts {
 	return prometheus.HistogramOpts{
-		Namespace: namespace,
-		Subsystem: subsystem,
-		Name:      name,
-		Buckets:   buckets,
+		Namespace:   namespace,
+		Subsystem:   subsystem,
+		Name:        name,
+		ConstLabels: constLabels,
+		Buckets:     buckets,
 	}
 }
 

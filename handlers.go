ea93abef678a2ff5cfb7b9d7bc6673acef5fe847
Aarsh Shah <aarshkshah1992@gmail.com>
2019-07-10 21:56:33 +0800

    fixed issue with import formatting

--- handlers.go
+++ handlers.go
@@ -12,13 +12,13 @@ import (
 	"github.com/libp2p/go-libp2p-core/peerstore"
 	pstore "github.com/libp2p/go-libp2p-peerstore"
 
-	"github.com/gogo/protobuf/proto"
-	"github.com/ipfs/go-cid"
+	proto "github.com/gogo/protobuf/proto"
+	cid "github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
 	u "github.com/ipfs/go-ipfs-util"
 	pb "github.com/libp2p/go-libp2p-kad-dht/pb"
 	recpb "github.com/libp2p/go-libp2p-record/pb"
-	"github.com/whyrusleeping/base32"
+	base32 "github.com/whyrusleeping/base32"
 )
 
 // The number of closer peers to send on requests.

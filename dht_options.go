c98518490cc773f6b81133fe7a8e2808fa37e4e2
Steven Allen <steven@stebalien.com>
2020-06-10 11:18:16 -0700

    Revert "Peer Diversity in the Routing Table (#658)"
    This reverts commit df4f28c31b26057660370f1975f3f1f1fcc8d591.
    
    Ongoing work is being done in feat/dht-hardening-0.7 and this change isn't quite ready.

--- dht_options.go
+++ dht_options.go
@@ -4,18 +4,15 @@ import (
 	"fmt"
 	"time"
 
+	ds "github.com/ipfs/go-datastore"
+	dssync "github.com/ipfs/go-datastore/sync"
+	"github.com/ipfs/go-ipns"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/protocol"
 	"github.com/libp2p/go-libp2p-kad-dht/providers"
-
-	"github.com/libp2p/go-libp2p-kbucket/peerdiversity"
 	record "github.com/libp2p/go-libp2p-record"
-
-	ds "github.com/ipfs/go-datastore"
-	dssync "github.com/ipfs/go-datastore/sync"
-	"github.com/ipfs/go-ipns"
 )
 
 // ModeOpt describes what mode the dht should operate in
@@ -59,7 +56,6 @@ type config struct {
 		latencyTolerance    time.Duration
 		checkInterval       time.Duration
 		peerFilter          RouteTableFilterFunc
-		diversityFilter     peerdiversity.PeerIPGroupFilter
 	}
 
 	// set to true if we're operating in v1 dht compatible mode
@@ -404,16 +400,6 @@ func V1CompatibleMode(enable bool) Option {
 func BootstrapPeers(bootstrappers ...peer.AddrInfo) Option {
 	return func(c *config) error {
 		c.bootstrapPeers = bootstrappers
-		return nil
-	}
-}
-
-// RoutingTablePeerDiversityFilter configures the implementation of the `PeerIPGroupFilter` that will be used
-// to construct the diversity filter for the Routing Table.
-// Please see the docs for `peerdiversity.PeerIPGroupFilter` AND `peerdiversity.Filter` for more details.
-func RoutingTablePeerDiversityFilter(pg peerdiversity.PeerIPGroupFilter) Option {
-	return func(c *config) error {
-		c.routingTable.diversityFilter = pg
 		return nil
 	}
 }

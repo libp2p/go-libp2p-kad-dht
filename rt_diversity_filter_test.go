47d1a864da938de04a35937704b5f4ef8489c55e
Adin Schmahmann <adin.schmahmann@gmail.com>
2020-06-15 13:52:28 -0400

    fix rt diversity test

--- rt_diversity_filter_test.go
+++ rt_diversity_filter_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	kb "github.com/libp2p/go-libp2p-kbucket"
 	"github.com/libp2p/go-libp2p-kbucket/peerdiversity"
 	swarmt "github.com/libp2p/go-libp2p-swarm/testing"
 	bhost "github.com/libp2p/go-libp2p/p2p/host/basic"
@@ -51,10 +52,10 @@ func TestRTPeerDiversityFilter(t *testing.T) {
 	require.False(t, r.Allow(g2))
 }
 
-func TestRoutingTableEndToEnd(t *testing.T) {
+func TestRoutingTableEndToEndMaxPerCpl(t *testing.T) {
 	ctx := context.Background()
 	h := bhost.New(swarmt.GenSwarm(t, ctx, swarmt.OptDisableReuseport))
-	r := NewRTPeerDiversityFilter(h, 2, 3)
+	r := NewRTPeerDiversityFilter(h, 1, 2)
 
 	d, err := New(
 		ctx,
@@ -67,7 +68,61 @@ func TestRoutingTableEndToEnd(t *testing.T) {
 	)
 	require.NoError(t, err)
 
-	// only 3 peers per prefix for a cpl
+	var d2 *IpfsDHT
+	var d3 *IpfsDHT
+
+	for {
+		d2 = setupDHT(ctx, t, false)
+		if kb.CommonPrefixLen(d.selfKey, kb.ConvertPeerID(d2.self)) == 1 {
+			break
+		}
+	}
+
+	for {
+		d3 = setupDHT(ctx, t, false)
+		if kb.CommonPrefixLen(d.selfKey, kb.ConvertPeerID(d3.self)) == 1 {
+			break
+		}
+	}
+
+	// d2 will be allowed in the Routing table but
+	// d3 will not be allowed.
+	connectNoSync(t, ctx, d, d2)
+	require.Eventually(t, func() bool {
+		return d.routingTable.Find(d2.self) != ""
+	}, 1*time.Second, 100*time.Millisecond)
+
+	connectNoSync(t, ctx, d, d3)
+	time.Sleep(1 * time.Second)
+	require.Len(t, d.routingTable.ListPeers(), 1)
+	require.True(t, d.routingTable.Find(d3.self) == "")
+
+	// it works after removing d2
+	d.routingTable.RemovePeer(d2.self)
+	b, err := d.routingTable.TryAddPeer(d3.self, true, false)
+	require.NoError(t, err)
+	require.True(t, b)
+	require.Len(t, d.routingTable.ListPeers(), 1)
+	require.True(t, d.routingTable.Find(d3.self) != "")
+}
+
+func TestRoutingTableEndToEndMaxPerTable(t *testing.T) {
+	ctx := context.Background()
+	h := bhost.New(swarmt.GenSwarm(t, ctx, swarmt.OptDisableReuseport))
+	r := NewRTPeerDiversityFilter(h, 100, 3)
+
+	d, err := New(
+		ctx,
+		h,
+		testPrefix,
+		NamespacedValidator("v", blankValidator{}),
+		Mode(ModeServer),
+		DisableAutoRefresh(),
+		RoutingTablePeerDiversityFilter(r),
+	)
+	require.NoError(t, err)
+
+	// only 3 peers per prefix for the table.
 	d2 := setupDHT(ctx, t, false, DisableAutoRefresh())
 	connect(t, ctx, d, d2)
 	waitForWellFormedTables(t, []*IpfsDHT{d}, 1, 1, 1*time.Second)
@@ -84,4 +139,5 @@ func TestRoutingTableEndToEnd(t *testing.T) {
 	connectNoSync(t, ctx, d, d5)
 	time.Sleep(1 * time.Second)
 	require.Len(t, d.routingTable.ListPeers(), 3)
+	require.True(t, d.routingTable.Find(d5.self) == "")
 }

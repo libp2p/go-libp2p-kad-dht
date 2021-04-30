86e473a288ecf2971d00144e43758b590f978549
Steven Allen <steven@stebalien.com>
2021-04-29 20:57:31 -0700

    chore: update deps
    - go-libp2p
    - protobuf (and regen)

--- dht_filters_test.go
+++ dht_filters_test.go
@@ -35,17 +35,17 @@ type mockConn struct {
 
 var _ network.Conn = (*mockConn)(nil)
 
-func (m *mockConn) ID() string                         { return "0" }
-func (m *mockConn) Close() error                       { return nil }
-func (m *mockConn) NewStream() (network.Stream, error) { return nil, nil }
-func (m *mockConn) GetStreams() []network.Stream       { return []network.Stream{} }
-func (m *mockConn) Stat() network.Stat                 { return network.Stat{Direction: network.DirOutbound} }
-func (m *mockConn) LocalMultiaddr() ma.Multiaddr       { return m.local.Addrs[0] }
-func (m *mockConn) RemoteMultiaddr() ma.Multiaddr      { return m.remote.Addrs[0] }
-func (m *mockConn) LocalPeer() peer.ID                 { return m.local.ID }
-func (m *mockConn) LocalPrivateKey() ic.PrivKey        { return nil }
-func (m *mockConn) RemotePeer() peer.ID                { return m.remote.ID }
-func (m *mockConn) RemotePublicKey() ic.PubKey         { return nil }
+func (m *mockConn) ID() string                                        { return "0" }
+func (m *mockConn) Close() error                                      { return nil }
+func (m *mockConn) NewStream(context.Context) (network.Stream, error) { return nil, nil }
+func (m *mockConn) GetStreams() []network.Stream                      { return []network.Stream{} }
+func (m *mockConn) Stat() network.Stat                                { return network.Stat{Direction: network.DirOutbound} }
+func (m *mockConn) LocalMultiaddr() ma.Multiaddr                      { return m.local.Addrs[0] }
+func (m *mockConn) RemoteMultiaddr() ma.Multiaddr                     { return m.remote.Addrs[0] }
+func (m *mockConn) LocalPeer() peer.ID                                { return m.local.ID }
+func (m *mockConn) LocalPrivateKey() ic.PrivKey                       { return nil }
+func (m *mockConn) RemotePeer() peer.ID                               { return m.remote.ID }
+func (m *mockConn) RemotePublicKey() ic.PubKey                        { return nil }
 
 func TestFilterCaching(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
--- pb/dht.pb.go
+++ pb/dht.pb.go
@@ -721,10 +721,7 @@ func (m *Message) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthDht
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthDht
 			}
 			if (iNdEx + skippy) > l {
@@ -859,10 +856,7 @@ func (m *Message_Peer) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthDht
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthDht
 			}
 			if (iNdEx + skippy) > l {

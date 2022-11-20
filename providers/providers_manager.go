9896ce5b196a4c262d489b35460056a4b4e5618f
Marten Seemann <martenseemann@gmail.com>
2022-11-21 09:32:06 +1300

    define the ProviderAddrTTL in this repo (#797)

--- providers/providers_manager.go
+++ providers/providers_manager.go
@@ -25,6 +25,11 @@ import (
 // keys stored in the data store.
 const ProvidersKeyPrefix = "/providers/"
 
+// ProviderAddrTTL is the TTL of an address we've received from a provider.
+// This is also a temporary address, but lasts longer. After this expires,
+// the records we return will require an extra lookup.
+const ProviderAddrTTL = time.Minute * 30
+
 // ProvideValidity is the default time that a provider record should last
 var ProvideValidity = time.Hour * 24
 var defaultCleanupInterval = time.Hour
@@ -232,7 +237,7 @@ func (pm *ProviderManager) run(ctx context.Context, proc goprocess.Process) {
 // AddProvider adds a provider
 func (pm *ProviderManager) AddProvider(ctx context.Context, k []byte, provInfo peer.AddrInfo) error {
 	if provInfo.ID != pm.self { // don't add own addrs.
-		pm.pstore.AddAddrs(provInfo.ID, provInfo.Addrs, peerstore.ProviderAddrTTL)
+		pm.pstore.AddAddrs(provInfo.ID, provInfo.Addrs, ProviderAddrTTL)
 	}
 	prov := &addProv{
 		ctx: ctx,

254dcbc4c634e0b4fc362dae76afc81ff645efbe
guillaumemichel <guillaume@michel.id>
2025-07-11 23:01:15 +0200

    connectivity package

--- provider/internal/connectivity/connectivity.go
+++ provider/internal/connectivity/connectivity.go
@@ -1,4 +1,4 @@
-package provider
+package connectivity
 
 import (
 	"context"
@@ -9,7 +9,7 @@ import (
 	"github.com/filecoin-project/go-clock"
 )
 
-// connectivityChecker provides a thread-safe way to verify the connectivity of
+// ConnectivityChecker provides a thread-safe way to verify the connectivity of
 // a node, and triggers a wake-up callback when the node comes back online
 // after a period offline.
 //
@@ -22,7 +22,7 @@ import (
 //     connectivity is restored.
 //     – Once back online, the node’s status is updated and `backOnlineNotify`
 //     is invoked exactly once.
-type connectivityChecker struct {
+type ConnectivityChecker struct {
 	ctx context.Context
 
 	online atomic.Bool
@@ -37,11 +37,40 @@ type connectivityChecker struct {
 	backOnlineNotify func()                     // callback when node comes back online
 }
 
-func (c *connectivityChecker) isOnline() bool {
+// New creates a new ConnectivityChecker instance.
+func New(ctx context.Context, checkFunc func(context.Context) bool, backOnlineNotify func(), opts ...Option) (*ConnectivityChecker, error) {
+	var cfg config
+	err := cfg.apply(append([]Option{DefaultConfig}, opts...)...)
+	if err != nil {
+		return nil, err
+	}
+	c := &ConnectivityChecker{
+		ctx:                  ctx,
+		clock:                cfg.clock,
+		onlineCheckInterval:  cfg.onlineCheckInterval,
+		offlineCheckInterval: cfg.offlineCheckInterval,
+		checkFunc:            checkFunc,
+		backOnlineNotify:     backOnlineNotify,
+	}
+	c.online.Store(true) // Start with the node considered online
+
+	return c, nil
+}
+
+// IsOnline returns true if the node is currently online, false otherwise.
+func (c *ConnectivityChecker) IsOnline() bool {
 	return c.online.Load()
 }
 
-func (c *connectivityChecker) triggerCheck() {
+// TriggerCheck triggers an asynchronous connectivity check.
+//
+// * If a check is already running, does nothing.
+// * If a check was already performed within the last `onlineCheckInterval`, does nothing.
+// * If after running the check the node is still online, update the last check timestamp.
+// * If the node is found offline, enter the loop:
+//   - Perform connectivity check every `offlineCheckInterval`.
+//   - When node is found back online, run the `backOnlineNotify` callback.
+func (c *ConnectivityChecker) TriggerCheck() {
 	if c.ctx.Err() != nil {
 		// Noop
 		return

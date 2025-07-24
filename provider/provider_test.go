b07953eda0a3a62fa684f4e3ea10eb25eda89621
guillaumemichel <guillaume@michel.id>
2025-07-24 12:00:31 +0200

    add some tests

---
+++ provider/provider_test.go
@@ -0,0 +1,127 @@
+package provider
+
+import (
+	"context"
+	"crypto/sha256"
+	"errors"
+	"strconv"
+	"testing"
+
+	kb "github.com/libp2p/go-libp2p-kbucket"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/peer"
+	mh "github.com/multiformats/go-multihash"
+	"github.com/probe-lab/go-libdht/kad/key/bit256"
+
+	"github.com/probe-lab/go-libdht/kad/trie"
+
+	pb "github.com/libp2p/go-libp2p-kad-dht/pb"
+	"github.com/libp2p/go-libp2p-kad-dht/provider/internal/keyspace"
+
+	"github.com/stretchr/testify/require"
+)
+
+func genMultihashes(n int) []mh.Multihash {
+	mhs := make([]mh.Multihash, n)
+	var err error
+	for i := range n {
+		mhs[i], err = mh.Sum([]byte(strconv.Itoa(i)), mh.SHA2_256, -1)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return mhs
+}
+
+func genRandPeerID(t *testing.T) peer.ID {
+	_, pub, err := crypto.GenerateKeyPair(crypto.Ed25519, -1)
+	require.NoError(t, err)
+	pid, err := peer.IDFromPublicKey(pub)
+	require.NoError(t, err)
+	return pid
+}
+
+var _ pb.MessageSender = (*mockMsgSender)(nil)
+
+type mockMsgSender struct {
+	sendMessageFunc func(ctx context.Context, p peer.ID, m *pb.Message) error
+}
+
+func (ms *mockMsgSender) SendRequest(ctx context.Context, p peer.ID, m *pb.Message) (*pb.Message, error) {
+	// Unused
+	return nil, nil
+}
+
+func (ms *mockMsgSender) SendMessage(ctx context.Context, p peer.ID, m *pb.Message) error {
+	if ms.sendMessageFunc == nil {
+		return nil
+	}
+	return ms.sendMessageFunc(ctx, p, m)
+}
+
+func TestProvideKeysToPeer(t *testing.T) {
+	msgCount := 0
+	msgSender := &mockMsgSender{
+		sendMessageFunc: func(ctx context.Context, p peer.ID, m *pb.Message) error {
+			msgCount++
+			return errors.New("error")
+		},
+	}
+	reprovider := SweepingProvider{
+		msgSender: msgSender,
+	}
+
+	nKeys := 16
+	pid, err := peer.Decode("12BoooooPEER")
+	require.NoError(t, err)
+	mhs := genMultihashes(nKeys)
+	pmes := &pb.Message{}
+
+	// All ADD_PROVIDER RPCs fail, return an error after reprovideInitialFailuresAllowed+1 attempts
+	err = reprovider.provideKeysToPeer(pid, mhs, pmes)
+	require.Error(t, err)
+	require.Equal(t, maxConsecutiveProvideFailuresAllowed+1, msgCount)
+
+	// Only fail 33% of requests. The operation should be considered a success.
+	msgCount = 0
+	msgSender.sendMessageFunc = func(ctx context.Context, p peer.ID, m *pb.Message) error {
+		msgCount++
+		if msgCount%3 == 0 {
+			return errors.New("error")
+		}
+		return nil
+	}
+	err = reprovider.provideKeysToPeer(pid, mhs, pmes)
+	require.NoError(t, err)
+	require.Equal(t, nKeys, msgCount)
+}
+
+func TestKeysAllocationsToPeers(t *testing.T) {
+	nKeys := 1024
+	nPeers := 128
+	replicationFactor := 10
+
+	mhs := genMultihashes(nKeys)
+	keysTrie := trie.New[bit256.Key, mh.Multihash]()
+	for _, c := range mhs {
+		keysTrie.Add(keyspace.MhToBit256(c), c)
+	}
+	peers := make([]peer.ID, nPeers)
+	peersTrie := trie.New[bit256.Key, peer.ID]()
+	for i := range peers {
+		peers[i] = genRandPeerID(t)
+		peersTrie.Add(keyspace.PeerIDToBit256(peers[i]), peers[i])
+	}
+	keysAllocations := keyspace.AllocateToKClosest(keysTrie, peersTrie, replicationFactor)
+
+	for _, c := range mhs {
+		k := sha256.Sum256(c)
+		closestPeers := kb.SortClosestPeers(peers, k[:])[:replicationFactor]
+		for _, p := range closestPeers[:replicationFactor] {
+			require.Contains(t, keysAllocations[p], c)
+		}
+		for _, p := range closestPeers[replicationFactor:] {
+			require.NotContains(t, keysAllocations[p], c)
+		}
+	}
+}

74c2269f426657a596d2434115232338c85a3229
Guillaume Michel <guillaumemichel@users.noreply.github.com>
2025-07-24 10:28:14 +0200

    provider: NextNonEmptyLeaf trie helper (#1106)
    * provider: simple key helpers
    
    * provider: AllEntries and AllValues helpers
    
    * provider: trie helper FindPrefixOfKey
    
    * provider: find subtrie
    
    * provider: NextNonEmptyLeaf trie helper
    
    * Update provider/internal/helpers/key.go
    
    Co-authored-by: Andrew Gillis <11790789+gammazero@users.noreply.github.com>
    
    * provider: trie helper FindPrefixOfKey
    
    ---------
    
    Co-authored-by: Andrew Gillis <11790789+gammazero@users.noreply.github.com>

--- provider/internal/helpers/trie.go
+++ provider/internal/helpers/trie.go
@@ -74,3 +74,73 @@ func FindSubtrie[K0 kad.Key[K0], K1 kad.Key[K1], D any](t *trie.Trie[K0, D], k K
 	}
 	return branch, !branch.IsEmptyLeaf()
 }
+
+// NextNonEmptyLeaf returns the leaf following the provided key `k` in the trie
+// according to the provided `order`.
+func NextNonEmptyLeaf[K0 kad.Key[K0], K1 kad.Key[K1], D any](t *trie.Trie[K0, D], k K0, order K1) *trie.Entry[K0, D] {
+	return nextNonEmptyLeafAtDepth(t, k, order, 0, false)
+}
+
+// nextNonEmptyLeafAtDepth is a recursive function that finds the non empty
+// leaf right after the supplied key `k`.
+//
+// We first need to go down the trie until we find the supplied `k` (if it
+// exists). Once we have found the key, or its closest neighbor (if it doesn't
+// exist) we hit the bottom of the trie. Then we go back up in the trie until
+// we are able to go deeper again following `order` until we fall on a
+// non-empty leaf. This is the leaf we are looking for.
+func nextNonEmptyLeafAtDepth[K0 kad.Key[K0], K1 kad.Key[K1], D any](t *trie.Trie[K0, D], k K0, order K1, depth int, hitBottom bool) *trie.Entry[K0, D] {
+	if hitBottom {
+		if t.IsNonEmptyLeaf() {
+			// Found the next non-empty leaf.
+			return &trie.Entry[K0, D]{Key: *t.Key(), Data: t.Data()}
+		}
+		if t.IsEmptyLeaf() {
+			return nil
+		}
+		// Going down the trie, looking for next non-empty leaf according to order.
+		orderBit := int(order.Bit(depth))
+		nextLeaf := nextNonEmptyLeafAtDepth(t.Branch(orderBit), k, order, depth+1, true)
+		if nextLeaf != nil {
+			return nextLeaf
+		}
+		return nextNonEmptyLeafAtDepth(t.Branch(1-orderBit), k, order, depth+1, true)
+	}
+
+	if t.IsLeaf() {
+		// We have reached the bottom of the trie at k or its closest leaf
+		if t.HasKey() {
+			if depth == 0 {
+				// Depth is 0, meaning there is a single key in the trie.
+				return &trie.Entry[K0, D]{Key: *t.Key(), Data: t.Data()}
+			}
+			cpl := k.CommonPrefixLength(*t.Key())
+			if cpl < k.BitLen() && cpl < order.BitLen() && order.Bit(cpl) == k.Bit(cpl) {
+				// k is closer to order than t.Key, so t.Key AFTER k, return it
+				return &trie.Entry[K0, D]{Key: *t.Key(), Data: t.Data()}
+			}
+		}
+		return nil
+	}
+	kBit := int(k.Bit(depth))
+	// Recursive call until we hit the bottom of the trie.
+	nextLeaf := nextNonEmptyLeafAtDepth(t.Branch(kBit), k, order, depth+1, false)
+	if nextLeaf != nil {
+		// Branch has found the next leaf, return it.
+		return nextLeaf
+	}
+	orderBit := int(order.Bit(depth))
+	if kBit == orderBit || depth == 0 {
+		// Neighbor branch is up next, according to order.
+		nextLeaf = nextNonEmptyLeafAtDepth(t.Branch(1-kBit), k, order, depth+1, true)
+		if nextLeaf != nil {
+			return nextLeaf
+		}
+		if depth == 0 {
+			// We have reached the end of the trie, start again from the first leaf.
+			return nextNonEmptyLeafAtDepth(t.Branch(kBit), k, order, depth+1, true)
+		}
+	}
+	// Next leaf not found, signal it to parent by returning an empty entry.
+	return nil
+}
--- provider/internal/helpers/trie_test.go
+++ provider/internal/helpers/trie_test.go
@@ -1,8 +1,13 @@
 package helpers
 
 import (
+	"crypto/rand"
+	"fmt"
+	"sort"
 	"testing"
 
+	"github.com/probe-lab/go-libdht/kad/key"
+	"github.com/probe-lab/go-libdht/kad/key/bit256"
 	"github.com/probe-lab/go-libdht/kad/key/bitstr"
 	"github.com/probe-lab/go-libdht/kad/trie"
 
@@ -138,4 +143,211 @@ func TestFindSubtrie(t *testing.T) {
 	require.False(t, ok)
 	_, ok = FindSubtrie(tr, bitstr.Key("00000"))
 	require.False(t, ok)
+}
+
+func TestNextNonEmptyLeafFullTrie(t *testing.T) {
+	bitlen := 4
+
+	tr := trie.New[bitstr.Key, any]()
+	nKeys := 1 << bitlen
+	binaryKeys := make([]bitstr.Key, 0, nKeys)
+	for i := range nKeys {
+		binary := fmt.Sprintf("%0*b", bitlen, i)
+		k := bitstr.Key(binary)
+		tr.Add(k, struct{}{})
+		binaryKeys = append(binaryKeys, k)
+	}
+
+	order := binaryKeys[0]
+	t.Run("OrderZero", func(t *testing.T) {
+		for i, k := range binaryKeys {
+			nextKey := NextNonEmptyLeaf(tr, k, order).Key
+			require.Equal(t, binaryKeys[(i+1)%nKeys], nextKey)
+		}
+	})
+
+	t.Run("Cycle", func(t *testing.T) {
+		initialKey := binaryKeys[0]
+		k := initialKey
+		for range binaryKeys {
+			k = NextNonEmptyLeaf(tr, k, order).Key
+		}
+		require.Equal(t, initialKey, k)
+	})
+
+	order = binaryKeys[nKeys-1]
+	t.Run("CustomOrder", func(t *testing.T) {
+		for i, k := range binaryKeys {
+			nextKey := NextNonEmptyLeaf(tr, k, order).Key
+			require.Equal(t, binaryKeys[(i-1+nKeys)%nKeys], nextKey)
+		}
+	})
+}
+
+func TestNextNonEmptyLeafSparseTrie(t *testing.T) {
+	bitlen := 10
+	sparsity := 4
+
+	tr := trie.New[bitstr.Key, any]()
+	nKeys := 1 << (bitlen - sparsity)
+	binaryKeys := make([]bitstr.Key, 0, nKeys)
+	suffix := (1 << sparsity) - 1
+	for i := range nKeys {
+		binary := fmt.Sprintf("%0*b", bitlen, i*(1<<sparsity)+suffix)
+		k := bitstr.Key(binary)
+		tr.Add(k, struct{}{})
+		binaryKeys = append(binaryKeys, k)
+	}
+
+	order := bitstr.Key(fmt.Sprintf("%0*b", bitlen, 0))
+	t.Run("OrderZero", func(t *testing.T) {
+		for i, k := range binaryKeys {
+			nextKey := NextNonEmptyLeaf(tr, k, order).Key
+			require.Equal(t, binaryKeys[(i+1)%nKeys], nextKey)
+		}
+	})
+
+	t.Run("MissingKey", func(t *testing.T) {
+		for i := range 1 << bitlen {
+			binary := fmt.Sprintf("%0*b", bitlen, i)
+			k := bitstr.Key(binary)
+			nextKey := NextNonEmptyLeaf(tr, k, order).Key
+			require.Equal(t, binaryKeys[((i+1)%(1<<bitlen))/(1<<sparsity)], nextKey, k)
+		}
+	})
+
+	order = binaryKeys[nKeys-1]
+	t.Run("CustomOrder", func(t *testing.T) {
+		for i, k := range binaryKeys {
+			nextKey := NextNonEmptyLeaf(tr, k, order).Key
+			require.Equal(t, binaryKeys[(i-1+nKeys)%nKeys], nextKey)
+		}
+	})
+}
+
+func TestNextNonEmptyLeafRandom(t *testing.T) {
+	order := bit256.ZeroKey()
+	nKeys := 256
+	tr := trie.New[bit256.Key, struct{}]()
+	keys := make([]bit256.Key, 0, nKeys)
+
+	var b [32]byte
+	for range nKeys {
+		if _, err := rand.Read(b[:]); err != nil {
+			require.NoError(t, err)
+		}
+		k := bit256.NewKey(b[:])
+		tr.Add(k, struct{}{})
+		keys = append(keys, k)
+
+		// Sort keys
+		sort.Slice(keys, func(i, j int) bool {
+			return keys[i].Compare(keys[j]) < 0
+		})
+
+		currentKey := bit256.ZeroKey()
+		for j, k := range keys {
+			currentKey = NextNonEmptyLeaf(tr, currentKey, order).Key
+			require.Equal(t, k, currentKey, "failed at index %d\nExp: %s\nGot: %s", j, key.BitString(k), key.BitString(currentKey))
+		}
+	}
+}
+
+func TestSimpleNextNonEmptyLeaf(t *testing.T) {
+	keys := []bitstr.Key{"00", "01", "10", "11"}
+	tr := trie.New[bitstr.Key, any]()
+
+	// Zero key in the trie
+	for _, k0 := range keys {
+		for _, k1 := range keys {
+			require.Nil(t, NextNonEmptyLeaf(tr, k0, k1))
+		}
+	}
+
+	// One key in the trie
+	for _, k0 := range keys {
+		tr.Add(k0, struct{}{})
+		require.Equal(t, 1, tr.Size())
+		for _, k1 := range keys {
+			for _, k2 := range keys {
+				require.Equal(t, k0, NextNonEmptyLeaf(tr, k1, k2).Key)
+			}
+		}
+		tr.Remove(k0)
+	}
+
+	// Two keys in the trie
+	for _, k0 := range keys {
+		for _, k1 := range keys {
+			if k0 == k1 {
+				continue
+			}
+			tr.Add(k0, struct{}{})
+			tr.Add(k1, struct{}{})
+			require.Equal(t, 2, tr.Size())
+
+			for i := range keys {
+				var expectedKey bitstr.Key
+				for j := range keys {
+					if keys[(i+j+1)%len(keys)] == k0 {
+						expectedKey = k0
+						break
+					}
+					if keys[(i+j+1)%len(keys)] == k1 {
+						expectedKey = k1
+						break
+					}
+				}
+				require.Equal(t, expectedKey, NextNonEmptyLeaf(tr, keys[i], keys[0]).Key, "leaf after %s should be %s", keys[i], expectedKey)
+			}
+
+			tr.Remove(k0)
+			tr.Remove(k1)
+		}
+	}
+
+	// Three keys in the trie
+	for i := range keys {
+		currentKeys := []bitstr.Key{}
+		for j := range keys {
+			if i == j {
+				continue
+			}
+			tr.Add(keys[j], struct{}{})
+			currentKeys = append(currentKeys, keys[j])
+		}
+		require.Equal(t, 3, tr.Size())
+		for j := range keys {
+			var expectedKey bitstr.Key
+		outerLoop:
+			for q := range keys {
+				for _, k := range currentKeys {
+					if keys[(j+q+1)%len(keys)] == k {
+						expectedKey = k
+						break outerLoop
+					}
+				}
+			}
+			require.Equal(t, expectedKey, NextNonEmptyLeaf(tr, keys[j], keys[0]).Key, "leaf after %s should be %s", keys[j], expectedKey)
+		}
+
+		for j := range keys {
+			if i == j {
+				continue
+			}
+			tr.Remove(keys[j])
+		}
+	}
+
+	// Four keys in the trie (only order "00")
+	for _, k := range keys {
+		tr.Add(k, struct{}{})
+	}
+	require.Equal(t, 4, tr.Size())
+	for i := range keys {
+		require.Equal(t, keys[(i+1)%len(keys)], NextNonEmptyLeaf(tr, keys[i], keys[0]).Key)
+	}
+	for _, k := range keys {
+		tr.Remove(k)
+	}
 }

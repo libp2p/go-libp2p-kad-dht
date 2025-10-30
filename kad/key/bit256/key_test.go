df3e0eaf7128af380d67ad6c1b7e0b96a86be42d
Guillaume Michel <guillaumemichel@users.noreply.github.com>
2025-10-30 14:37:49 +0100

    feat(key/bit256): memory optimized constructor (#25)
    * feat(key/bit256): memory optimized constructor
    
    * tests
    
    * minor quality improvements

--- kad/key/bit256/key_test.go
+++ kad/key/bit256/key_test.go
@@ -15,7 +15,7 @@ func TestKey(t *testing.T) {
 		Key1: NewKey(append(make([]byte, 31), 0x01)),
 
 		// key2 is key0 + 2 (00000...010)
-		Key2: NewKey(append(make([]byte, 31), 0x02)),
+		Key2: NewKeyFromArray([32]byte{31: 0x02}),
 
 		// key1xor2 is key1 ^ key2 (00000...011)
 		Key1xor2: NewKey(append(make([]byte, 31), 0x03)),
@@ -24,7 +24,7 @@ func TestKey(t *testing.T) {
 		Key100: NewKey(append([]byte{0x80}, make([]byte, 31)...)),
 
 		// key010 is key0 with the second most significant bit set (01000...000)
-		Key010: NewKey(append([]byte{0x40}, make([]byte, 31)...)),
+		Key010: NewKeyFromArray([32]byte{0: 0x40}),
 
 		KeyX: NewKey(append([]byte{0x23, 0xe4, 0xdd, 0x03}, make([]byte, 28)...)),
 	}

c7bb58684f8ac7418bc43046ae725febf7a0f013
guillaumemichel <guillaume@michel.id>
2025-10-30 21:33:53 +0100

    AllEntries depends on EntriesIter

--- provider/internal/keyspace/trie.go
+++ provider/internal/keyspace/trie.go
@@ -19,19 +19,14 @@ var zeroKey = bit256.ZeroKey()
 // AllEntries returns all entries (key + value) stored in the trie `t` sorted
 // by their keys in the supplied `order`.
 func AllEntries[K0 kad.Key[K0], K1 kad.Key[K1], D any](t *trie.Trie[K0, D], order K1) []trie.Entry[K0, D] {
-	return allEntriesAtDepth(t, order, 0)
-}
-
-func allEntriesAtDepth[K0 kad.Key[K0], K1 kad.Key[K1], D any](t *trie.Trie[K0, D], order K1, depth int) []trie.Entry[K0, D] {
 	if t == nil || t.IsEmptyLeaf() {
-		return nil
+		return []trie.Entry[K0, D]{}
 	}
-	if t.IsNonEmptyLeaf() {
-		return []trie.Entry[K0, D]{{Key: *t.Key(), Data: t.Data()}}
+	result := make([]trie.Entry[K0, D], 0, t.Size())
+	for entry := range EntriesIter(t, order) {
+		result = append(result, entry)
 	}
-	b := int(order.Bit(depth))
-	return append(allEntriesAtDepth(t.Branch(b), order, depth+1),
-		allEntriesAtDepth(t.Branch(1-b), order, depth+1)...)
+	return result
 }
 
 // trieIter is a generic helper that iterates over the trie and yields values
@@ -82,12 +77,14 @@ func EntriesIter[K0 kad.Key[K0], K1 kad.Key[K1], D any](t *trie.Trie[K0, D], ord
 // AllValues returns all values stored in the trie `t` sorted by their keys in
 // the supplied `order`.
 func AllValues[K0 kad.Key[K0], K1 kad.Key[K1], D any](t *trie.Trie[K0, D], order K1) []D {
-	entries := AllEntries(t, order)
-	out := make([]D, len(entries))
-	for i, entry := range entries {
-		out[i] = entry.Data
+	if t == nil || t.IsEmptyLeaf() {
+		return []D{}
 	}
-	return out
+	result := make([]D, 0, t.Size())
+	for value := range ValuesIter(t, order) {
+		result = append(result, value)
+	}
+	return result
 }
 
 // ValuesIter returns an iterator over all values stored in the trie `t`
